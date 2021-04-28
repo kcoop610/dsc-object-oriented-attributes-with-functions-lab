158e8ecd0b568ad54c4bc90db54496384962b79c
Kristin Cooper <kcoop610@gmail.com>
2021-04-28 12:46:27 -0500

    completed lab

--- school.py
+++ school.py
@@ -1,1 +1,19 @@
 class School:
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+    
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+
+    def grade(self, grade):
+        return self.roster[grade]
+
+    def sort_roster(self):
+        sorted = self.roster
+        for key in sorted:
+            sorted[key].sort()
+        return sorted
